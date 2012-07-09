3e24920ef0b01c79ce2d8e1ba57c0422e23dd203
edwardt.tril@gmail.com <edwardt.tril@gmail.com>
2012-07-09 12:00:52 -0700

    relocate files for better categoritzation

---
+++ OpenStack.Nova.ServiceModel/Operations/Flavor/GetFlavor.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OpenStack.Nova.ServiceModel
+{
+	public class GetFlavor
+	{
+		public GetFlavor ()
+		{
+		}
+	}
+}
+
---
+++ OpenStack.Nova.ServiceModel/Operations/Image/GetImageInfo.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OpenStack.Nova.ServiceModel
+{
+	public class GetImageInfo
+	{
+		public GetImageInfo ()
+		{
+		}
+	}
+}
+
