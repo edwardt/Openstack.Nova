f4259b0eb73c95b1e3507cc0f9f2f4b35db4969d
edwardt.tril@gmail.com <edwardt.tril@gmail.com>
2012-07-09 12:32:25 -0700

    add console

---
+++ Openstack.Nova.Client.Console/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Openstack.Nova.Client.Console
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
